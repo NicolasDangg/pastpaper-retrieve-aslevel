b19422385fe77868d4f36a02a2f3a9d2d279dca3
NicolasDanggg <nicolasdang57@gmail.com>
2026-02-01 08:48:09 +0700

    updating util files

--- pastpaper/utils.py
+++ pastpaper/utils.py
@@ -11,3 +11,13 @@ def sanitize_filename(name: str) -> str:
     name = re.sub(r"[^\w\s.-]", "", name)
     name = re.sub(r"\s+", "_", name)
     return name
+
+
+def menu(title, options):
+    print(f"\n{title}")
+    for key, value in options.items():
+        if isinstance(value, tuple):
+            print(f"{key}) {value[0]} ({value[1]})")
+        else:
+            print(f"{key}) {value}")
+    return input("> ").strip()
